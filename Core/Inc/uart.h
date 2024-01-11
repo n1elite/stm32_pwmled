3a4d52253d10c9e67d395e4e1ae8489df9ca574a
n1elite <junho990613@gmail.com>
2024-01-11 16:10:42 +0900

    이것은 첫번째 프로젝트입니다.

---
+++ Core/Inc/uart.h
@@ -0,0 +1,15 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jan 10, 2024
+ *      Author: junho
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+
+#include "main.h"
+
+void initUart(UART_HandleTypeDef *);
+
+#endif /* INC_UART_H_ */
---
+++ Core/Src/uart.c
@@ -0,0 +1,21 @@
+/*
+ * uart.c
+ *
+ *  Created on: Jan 10, 2024
+ *      Author: junho
+ */
+
+#include "uart.h"
+
+UART_HandleTypeDef *huart;
+
+void initUart (UART_HandleTypeDef *inHuart){
+	huart=inHuart;
+
+}
+
+
+int _write(int file, char *p, int len) {
+   HAL_UART_Transmit(huart, (uint8_t *)p, len, len);
+   return len;
+}
